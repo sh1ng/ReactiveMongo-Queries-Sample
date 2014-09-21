5eb5808c4edda3eb20847b6425f5d45fea29b839
sh1ng <vladimir.ovsyannikov@gmail.com>
2014-09-21 11:17:51 +0200

    no message

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.joda.time._
+import reactivemongo.queries.Query._
+import reactivemongo.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.bson._
 
 object Application extends Controller {
+  val driver = new MongoDriver
+  val connection = driver.connection(List("localhost"))
+  val db = connection("reactivemongoqueries")
+  val collection = db("tasks")
+  
+  implicit object dateTimeHandler extends BSONReader[BSONDateTime, DateTime] with BSONWriter[DateTime, BSONDateTime] {
+	    def read(b: BSONDateTime) = new DateTime(b.value)
+	    def write(value: DateTime) = BSONDateTime(value.getMillis)
+	}
+  
+  implicit val taskHandler = Macros.handler[Task]
+  
+  case class Task(id: String, label: String, text: String, createdAt: DateTime, actionDate: DateTime)
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action.async {
+    for{
+      tasks <- collection.find(BSONDocument(""->"")).sort(on[Task].sortAsc(_.actionDate)).cursor[Task].collect[List]()
+    } yield Ok(views.html.index(tasks))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -2,10 +2,13 @@ name := "sample"
 
 version := "1.0-SNAPSHOT"
 
+resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "com.github.ReactiveMongo-Queries" %% "reactivemongo-queries" % "0.10.0.a-SNAPSHOT"
 )     
 
 play.Project.playScalaSettings
